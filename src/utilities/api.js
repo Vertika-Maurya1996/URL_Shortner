6852734ff02f0b5ae74808a751552b9fbc65114f
Vertika-Maurya1996 <97334010+Vertika-Maurya1996@users.noreply.github.com>
2024-03-01 16:47:55 +0530

    Update api.js

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = "https://url-shortner-server-sigma.vercel.app/";
+const baseURL = "http://url-shortner-server-sigma.vercel.app/";
 const api = axios.create({baseURL:baseURL,timeout:5000,headers:{'Content-Type':'application/json'}})
 export default api
