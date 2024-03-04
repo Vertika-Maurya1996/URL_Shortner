ac6823cb1d6b4674c371e21b277544105f5fb006
Vertika-Maurya1996 <97334010+Vertika-Maurya1996@users.noreply.github.com>
2024-03-05 00:38:08 +0530

    Update api.js

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8000/";
+const baseURL = "https://verceltest-olive-kappa.vercel.app/";
 const api = axios.create({baseURL:baseURL,timeout:5000,headers:{'Content-Type':'application/json'}})
 export default api
